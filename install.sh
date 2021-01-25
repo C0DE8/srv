a346db28293d2fe8d30e01a824c7a0218033ab12
Björn Ellebrecht <github.bjoern@ellebrecht.name>
2021-01-25 09:55:32 +0100

    create install.sh
    basic installer

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+
+# Server Default Installer:
+# (c) 2021 C0DE8
+
+# Install with this command (from your Linux machine):
+#
+# curl -sSL https://install.c75x.de | bash
+
+# -e option instructs bash to immediately exit if any command [1] has a non-zero exit status
+# We do not want users to end up with a partially working install, so we exit the script
+# instead of continuing the installation with something broken
+set -e
+
+# Set PATH to a usual default to assure that all basic commands are available.
+# When using "su" an uncomplete PATH could be passed: https://github.com/pi-hole/pi-hole/issues/3209
+export PATH+=':/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+
+# config
+BASE_REPO_PATH="_srv-defaults"
+
+# clone main repository (bitbucket.org)
+git clone https://beitsolutions@bitbucket.org/beitsolutions/server-defaults.git "${BASE_REPO_PATH}"
+
+cd "${BASE_REPO_PATH}"
+./install.sh
